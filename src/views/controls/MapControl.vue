72d4f0da1574763b50d63954ce6fc34c64da5a9e
hackeriori <453843655@qq.com>
2026-08-03 11:27:59 +0800

    refactor(controls): 修复地图控制组件

--- src/views/controls/MapControl.vue
+++ src/views/controls/MapControl.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
 import {onMounted, onUnmounted, ref, type Ref, shallowRef, watch} from 'vue';
 import {BoxGeometry, Mesh, MeshBasicMaterial, Scene} from 'three';
-import {MapControls} from 'three/examples/jsm/controls/MapControls';
-import RendererHelper from '@/core/helpers/RendererHelper';
-import PerspectiveCameraHelper from '@/core/helpers/PerspectiveCameraHelper';
+import {MapControls} from 'three/examples/jsm/controls/MapControls.js';
+import RendererHelper from '../../core/helpers/RendererHelper';
+import PerspectiveCameraHelper from '../../core/helpers/PerspectiveCameraHelper';
 
 const el = shallowRef() as Ref<HTMLDivElement>;
 let rendererHelper: RendererHelper;
@@ -52,18 +52,23 @@ onUnmounted(() => {
 </script>
 
 <template>
-  <div class="height100 ls-abs-outer">
-    <div class="height100" ref="el">
+  <div class="h-full relative">
+    <div class="h-full" ref="el">
     </div>
-  </div>
-  <div class="controlBox">
-    <div>
-      屏幕方向平移
-      <el-switch v-model="screenSpacePanning"></el-switch>
-    </div>
-    <div>
-      缩放至鼠标所在位置
-      <el-switch v-model="zoomToCursor"></el-switch>
+    <div class="rounded-lg w-xs space-y-2 p-2 absolute left-2 top-2 bg-default">
+      <div>
+        <USwitch label="屏幕方向平移" v-model="screenSpacePanning"></USwitch>
+      </div>
+      <div class="text-sm text-muted">定义平移视图时相机的平移方式。如果设置为
+        true，相机将在屏幕空间（相机镜头的 XY 平面）中平移。否则，相机将在垂直于相机“向上方向”（Up
+        向量）的平面（XZ 平面）内平移。默认为 true。
+      </div>
+      <div>
+        <USwitch label="缩放至鼠标所在位置" v-model="zoomToCursor"></USwitch>
+      </div>
+      <div class="text-sm text-muted">将此属性设置为 true 时，可以实现以光标（鼠标指针）所在的位置为中心进行缩放。默认为
+        false。
+      </div>
     </div>
   </div>
 </template>
